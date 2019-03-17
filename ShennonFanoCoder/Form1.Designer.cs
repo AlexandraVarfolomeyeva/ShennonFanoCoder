f7cc5a05d302737489336522b9480b41b75ecc54
AlexandraVarfolomeyeva <AAV998@yandex.ru>
2019-03-17 11:47:09 +0300

    построение дерева

--- ShennonFanoCoder/Form1.Designer.cs
+++ ShennonFanoCoder/Form1.Designer.cs
@@ -28,13 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(56, 56);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(599, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseDoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(680, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 30);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Открыть";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(56, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(239, 22);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Выберете файл для сжатия";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(104, 328);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(104, 392);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- ShennonFanoCoder/Form1.cs
+++ ShennonFanoCoder/Form1.cs
@@ -10,11 +10,369 @@ using System.Windows.Forms;
 
 namespace ShennonFanoCoder
 {
+    
+
     public partial class Form1 : Form
     {
+        private OpenFileDialog openFileDialog1; //opening file
+        int N; //amount of symbols
+        public List<symbol> Cfile; //symbols of file to code in array = alphabet
+        char[] messfile; //the whole message from the file to code
+       // TreeView<symbol> Tree; //TreeNode -- node
+
+        public class Tree<T>
+        {
+            public int weight=0;//sums of parts of table
+            public Tree<T> Left, Right;
+            public T leave=default(T);//null
+            public Tree<T> parent;
+        }
+
+        public class symbol
+        {
+            public char ch; // the symbol itself
+            public int prob; // number of symbol and their probability
+            public char[] code; //the code they are going to have
+        }
+
+
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void textBox1_MouseDoubleClick(object sender, MouseEventArgs e) //open file
+        {
+
+            openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+            }
+
+            Opening();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+            }
+            Opening();
+        } //open file
+
+        void Opening()
+        {
+            Cfile = new List<symbol>();
+            System.IO.StreamReader sr = new
+                   System.IO.StreamReader(openFileDialog1.FileName); // got file to code
+            N = Convert.ToInt32(sr.BaseStream.Length); //got amount of symbols in file to code                                                             
+            sr.BaseStream.Position = 0;
+            messfile = new char[N];
+            label3.Text = "";
+            for (int i = 0; i < N; i++)
+            {
+                messfile[i] = Convert.ToChar(sr.Read());
+                label3.Text += messfile[i]; //getting the message
+                if (Cfile.Count == 0) //if first symbol
+                {
+                    symbol c = new symbol();
+                    c.ch = messfile[i];
+                    c.code = null;
+                    c.prob = 1;
+                    Cfile.Add(c);
+                }
+                else // if not
+                {
+                    bool done = false; // if the alphabet was renewed
+                    for (int j = 0; j < Cfile.Count; j++) //getting the alphabet
+                    {
+
+                        if (!done)
+                        {
+                            if (Cfile[j].ch == messfile[i]) // if exists 
+                            {
+                                Cfile[j].prob++;
+                                done = true;
+                            }
+                            else
+                            if (j == Cfile.Count - 1)
+                            {
+                                symbol c = new symbol();
+                                c.ch = messfile[i];
+                                c.code = null;
+                                c.prob = 1;
+                                Cfile.Add(c);
+                                done = true;
+                            }
+                        }
+                    }
+                }
+            }
+
+            sr.Close();
+            Sort();
+        }
+
+       
+
+        public class NameComparer : IComparer<symbol>
+        {
+            public int Compare(symbol k1, symbol k2)
+            {
+                if (k1.prob > k2.prob)
+                {
+                    return -1;
+                }
+                else if (k1.prob < k2.prob)
+                {
+                    return 1;
+                }
+
+                return 0;
+            }
+        }
+        void Sort()
+        {
+            NameComparer cn = new NameComparer();
+            Cfile.Sort(cn);
+            CreateTree();
+        }
+
+        void CreateTree()
+        {
+           Tree<symbol> tree = new Tree<symbol>();
+          
+            int sum=0;
+            for (int i = 0; i < Cfile.Count; i++)
+            {
+                sum += Cfile[i].prob;
+            }
+            tree.weight = sum;
+            tree.parent = null;
+            BuildingTree(0,Cfile.Count-1,sum,tree);
+        }
+
+        void BuildingTree(int Li, int Ri, int sum, Tree<symbol> node) 
+                                        //index Left, Right, Sum of all elements, parent
+        {
+            int Lb =Li, Rb = Ri;
+            int sumL= Cfile[0].prob, sumR= sum - Cfile[0].prob; //sums of the parts
+            for (int LeftIndex = Li;sumL<sumR;LeftIndex++) //while  sums are not close
+            {
+                sumL += Cfile[LeftIndex].prob;
+                sumR -= Cfile[LeftIndex].prob;
+                Ri--;
+                Li++;
+            }
+            Tree<symbol> node1 = new Tree<symbol>();
+            node1.weight = sumL;
+            Console.WriteLine("SumL -" + sumL);
+            Tree<symbol> node2 = new Tree<symbol>();
+            node2.weight = sumR;
+            Console.WriteLine("SumR -" + sumR);
+            node.Left = node1;
+            node.Right = node2;
+            node1.parent = node;
+            node2.parent = node;
+            if (Li -Lb <=0)
+            {
+                node1.leave = Cfile[Lb];
+                Console.WriteLine("leave L -" + node1.leave.ch);
+            }
+            else
+            {
+                BuildingTree(Lb, Li, sumL, node1);
+                Console.WriteLine("Recursion L -");
+            }
+            if (Rb - Ri <= 0)
+            {
+                node2.leave = Cfile[Rb];
+                Console.WriteLine("Leave R - " + node2.leave.ch);
+            }
+            else
+            {
+                BuildingTree(Ri, Rb, sumR, node2);
+                Console.WriteLine("Recursion R -");
+            }
+        }
+
+        /*  void Quick_sort(symbol[] b, char left, char right)
+          {
+              char i = left, j = right;
+              symbol swap;
+              //   int t = b[(i + j) >> 1].prob;
+              int t = b[0].prob;
+              while (i <= j)
+              {
+                  while (b[i].prob > t)
+                      i++;
+                  while (t > b[j].prob)
+                      j--;
+
+                  if (i <= j)
+                  {
+                      if ((b[i].prob < b[j].prob))
+                      {
+                          swap = b[i];
+                          b[i] = b[j];
+                          b[j] = swap;
+                      }
+
+                      i++;
+                      j--;
+                  }
+              }
+        
+              if (left < j)
+                  Quick_sort(b, left, j);
+              if (i < right)
+                  Quick_sort(b, i, right);
+          }
+          */
+
+        void Coding_Shannon(symbol[] a, int left, int right, int sum)
+        {
+            int rest = 0;     // Sum of probability in one half of the prob table
+            int merge = 0;     // Number of merge character
+            int fringe = 0;   // Merge probability
+
+            if (left != right)
+                if (right != 0)
+                {
+                    for (int i = left; i <= right; i++)
+                    {
+                        rest += a[i].prob; //Left part of the table
+                        if (a[i].prob > ((sum + 1) >> 1)) // Prob of symbol > sum/2, then usual table cannot be built => need handling
+                        {                                 // Here it is...
+                            //a[i].code.push_back(1);
+                            merge = i;
+                            fringe = a[i].prob;
+                            continue;
+                        }
+                        if (rest <= ((sum + 1) >> 1))     // Symbol is in the first half of the prob table
+                        {
+                            //a[i].code.push_back(1);       // Write '1'
+                            fringe = rest;
+                            merge = i;
+                        }
+                        else                              // Symbol is in the second half of the prob table
+                        {
+                            //a[i].code.push_back(0);       // Write '0'
+                        }
+                    }
+                    if (left <= merge && merge + 1 <= right)  // Divide and handle both halfs
+                    {
+                        Coding_Shannon(a, left, merge, fringe);
+                        Coding_Shannon(a, merge + 1, right, sum - fringe);
+                    }
+                }
+        }
+
+
+        //void FormHeader(FILE* f, symbol* &symb, unsigned short alpha)
+        //{
+        //    if (alpha == 1)
+        //    {
+        //        fwrite(&alpha, 1, 1, f);
+        //        fputc(symb[0].ch, f);
+        //        fputc(0, f);                        // Write pairs of type: <Count, Length>
+        //        fputc(1, f);
+        //        printf("\nHeader size 4\n");
+        //    }
+        //    else
+        //    {
+        //        fwrite(&alpha, 1, 1, f);            // Write the number of characters, used in the code table
+        //                                            // In another words - the alphabet of the message
+        //        for (unsigned short i = 0; i < alpha; i++)
+        //        {
+        //            fputc(symb[i].ch, f);           // Write sequentially symbols from the code table
+        //        }
+
+        //        unsigned int count = 0;             // This will count the number of codes with the same length in succession
+        //        unsigned int hsize = alpha;
+        //        for (unsigned short i = 0; i < alpha; i++)
+        //        {
+        //            while (symb[i].code.size() == symb[i + 1].code.size())
+        //            {
+        //                ++count;                    // Here compare code length of the neighbouring symbols
+        //                ++i;                        // and count them, if codes are of the same length
+        //            }
+        //            fputc(count, f);                // Write pairs of type: <Count, Length>
+        //            fputc(symb[i].code.size(), f);
+        //            hsize += 2;
+        //            count = 0;
+        //        }
+
+        //        printf("\nHeader size %d\n", hsize + 1);
+        //    }
+        //}
+
+
+        // -----------------------------------------------------------------------
+        // Write coded info into file
+
+        //void Pack(symbol* &symb, unsigned int size, unsigned char* &message, unsigned short alpha)
+        //{
+        //    unsigned char c;                  // Char to form a byte
+        //    unsigned char cnt = 0;
+        //    double aver = 0.0;
+        //    FILE* f = fopen("Target.b", "wb");
+
+        //    FormHeader(f, symb, alpha);       // Put the service info
+
+        //    // Here, coding the message
+        //    for (unsigned int k = 0; k < size; k++)
+        //    {
+        //        for (unsigned short i = 0; i < alpha; i++)
+        //        {
+        //            if (symb[i].ch != message[k]) continue;
+        //            for (vector < unsigned char >::iterator j = symb[i].code.begin(); j != symb[i].code.end(); j++)
+        //            {
+        //                if (*j == 1)
+        //                {
+        //                    c = (c << 1) | 1;      // Write '1'
+        //                    ++cnt;
+        //                }
+        //                else
+        //                {
+        //                    ++cnt;                 // Write '0'
+        //                    c <<= 1;
+        //                }
+        //                if (cnt == 7)              // Have a byte
+        //                {
+        //                    aver += 8;
+        //                    cnt = 0;
+        //                    fprintf(f, "%c", c);
+        //                    c = 0;
+        //                }
+        //            }
+        //        }
+        //    }
+        //    aver = aver + cnt + 1;
+        //    unsigned char shift = 0;
+        //    if (cnt != 0)
+        //    {
+        //        shift = 7 - cnt;
+        //        while (cnt != 7)
+        //        {
+        //            c <<= 1;
+        //            ++cnt;
+        //        }
+        //        fprintf(f, "%c", c);
+        //    }
+
+        //    fwrite(&shift, 1, 1, f);
+
+        //    printf("Average quantity of bits per symbol: %f\n", aver / ((float)size));
+
+        //    fclose(f);
+        //}
+
+
+
     }
 }
